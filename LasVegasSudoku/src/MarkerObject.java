8c503b3e7ce29639035d4722d5d05d93e597ec84
Fredrik Rosenqvist <frero718@student.liu.se>
2014-06-07 19:01:33 +0800

    first commit

---
+++ LasVegasSudoku/src/MarkerObject.java
@@ -0,0 +1,27 @@
+
+public class MarkerObject
+{
+	private int xVal,yVal,value;
+	
+	public MarkerObject(int value, int xVal, int yVal){
+		this.xVal=xVal;
+		this.yVal=yVal;
+		this.value=value;
+	}
+
+	public int getxVal()
+	{
+		return xVal;
+	}
+
+	public int getyVal()
+	{
+		return yVal;
+	}
+	
+	public int getValue()
+	{
+		return value;
+	}
+	
+}
